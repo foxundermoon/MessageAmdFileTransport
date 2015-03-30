d00d38ccca39eb8fc01ebb7fc095bb7c02bd030c
foxundermoon <fox@vvfox.com>
2015-03-30 15:49:07 +0800

    init client cli

---
+++ MVCserver/FileDownloadAndUpload/MongodbTest/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongodbTest {
+    public partial class Form1 : Form {
+        public Form1( ) {
+            InitializeComponent();
+        }
+
+        private async void button1_Click( object sender, EventArgs e ) {
+
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("foo");
+            var collection = database.GetCollection<BsonDocument>("bar");
+
+            await collection.InsertOneAsync(new BsonDocument("Name", "Jack"));
+
+
+            var list = await collection.Find(new BsonDocument("Name", "Jack"))
+                .ToListAsync();
+
+
+            foreach(var document in list) {
+                Console.WriteLine(document["Name"]);
+            }
+        }
+
+        private async void button2_Click( object sender, EventArgs e ) {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("jnsw");
+            var collection = database.GetCollection<BsonDocument>("user");
+            //for(var i=0; i<100; i++) {
+            //    var u = new  User();
+            //    u.Name=i+"";
+            //    u.Password="123456";
+            //     await collection.InsertOneAsync(u);
+            //}
+
+            User quser = new User { Password="123456" };
+            var pasd = new BsonString("123456");
+            var list = await collection.Find(qd =>qd["Password"] == pasd)
+                .ToListAsync();
+            Console.Write(list.ToJson());
+        }
+
+    }
+
+    public partial class User {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public byte[] RegTime { get; set; }
+        public Nullable<System.DateTime> LastLogin { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Nullable<int> status { get; set; }
+    }
+}
---
+++ MVCserver/FileDownloadAndUpload/MongodbTest/Program.cs
@@ -0,0 +1,98 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongodbTest {
+    class Program {
+        [STAThread]
+        static void Main( string[] args ) {
+            //System.Windows.Forms.Application.Run(new Form1());
+            MongodbTest();
+            //new Form1().Show();
+        }
+
+        private static void MongodbTest( ) {
+            var client = new MongoClient("mongodb://10.80.5.222:27017");
+            var database = client.GetDatabase("performTest_1000000");
+            var collection = database.GetCollection<BsonDocument>("test1");
+            try {
+                Console.WriteLine("start....");
+            var start = DateTime.UtcNow;
+            for(var i=1; i<1000000; i++) {
+                var dom = new BsonDocument { 
+                {"name","test name"},
+                {"number",i},
+                {"datetime",DateTime.UtcNow}
+                };
+               
+                collection.InsertOneAsync(dom);
+                
+            }
+            var stop =DateTime.UtcNow;
+            Console.WriteLine("complete");
+            Console.WriteLine("start :"+start.ToFileTimeUtc());
+            Console.WriteLine("stop:"+stop.ToFileTimeUtc());
+            Console.WriteLine("---------------------\n间隔  :"+(stop-start));
+            Console.WriteLine("second: stop->"+stop +" start->" + start);
+            Console.Read();
+            } catch(Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+       
+        //static void mongoDb( ) {
+        //    try {
+
+        //    var constr = "mongodb://localhost";
+        //    var client = new MongoClient();
+        //    var database = client.GetDatabase("test");
+        //    var collection = database.GetCollection<Entity>("entites");
+        //    for(var i=0; i<10; i++) {
+        //        var entity = new Entity { Name="tname",Nid=i, };
+        //        //await collection.InsertOneAsync(entity);
+        //        Console.WriteLine("add"+i +" ;"+entity.ToJson());
+        //    }
+
+        //    Console.WriteLine("add complete");
+
+        //    System.Threading.Thread.Sleep(1000);
+
+        //    var result = await collection.FindAsync<Entity>(e=>e.Name =="tname");
+        //        result.Start();
+        //        result.Wait();
+
+        //    var json=collection.ToJson();
+        //    foreach(var item in result.Result.Current) {
+        //        Console.WriteLine(item.ToJson());
+        //    }
+        //    var r = result.Result;
+
+        //    var j = r.ToJson();
+        //    //var query = new QueryDocument { };
+        //    } catch(Exception ex) {
+        //        Console.WriteLine(ex.ToString());
+        //    }
+        //    //var query = Query<Entity>()
+
+
+
+        //}
+    }
+
+
+    public class Entity {
+        public ObjectId Id { get; set; }
+        public int Nid { get; set; }
+
+        public string Name { get; set; }
+    }
+}
