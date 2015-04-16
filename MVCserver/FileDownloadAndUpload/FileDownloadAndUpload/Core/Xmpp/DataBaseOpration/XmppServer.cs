5e5bfedd84678717cc6d4ce96c1eae13d57c6e08
foxundermoon <fox@vvfox.com>
2015-04-16 15:30:36 +0800

    mysql database

---
+++ MVCserver/FileDownloadAndUpload/FileDownloadAndUpload/Core/Xmpp/DataBaseOpration/XmppServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using agsXMPP;
+using agsXMPP.protocol.client;
+using agsXMPP.Xml.Dom;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Collections.Concurrent;
+using System.Configuration;
+using FileDownloadAndUpload.Core.Config;
+using MongoDB.Bson;
+using FileDownloadAndUpload.Core.Utils;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Configuration;
+namespace FileDownloadAndUpload.Core.Xmpp {
+    partial class XmppServer {
+        //string conStr = "Server=10.80.5.222;Database=gis; User=otnp80;Password=123;Use Procedure Bodies=false;Charset=utf8;Allow Zero Datetime=True; Pooling=false; Max Pool Size=50; ";
+     
+    }
+}
